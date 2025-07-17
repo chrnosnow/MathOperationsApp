3d7f18e5e956b188afcbc8170fa9d76b7fb40798
Alex-Baba <alexbaba18@gmail.com>
2025-07-17 14:33:01 +0300

    testing database connection and added requirements.txt

---
+++ main.py
@@ -0,0 +1,18 @@
+# python
+from fastapi import FastAPI
+from db import create_db_and_tables
+from contextlib import asynccontextmanager
+
+
+#test for database connection and creation
+#to run use python -m uvicorn main:app --reload --port 8080
+# Define lifespan event handler for FastAPI
+@asynccontextmanager
+async def lifespan(app):
+    # Create database tables at startup
+    create_db_and_tables()
+    print("Database tables created successfully.")  # Confirm execution
+    yield
+
+# Create FastAPI application instance with lifespan handler
+app = FastAPI(lifespan=lifespan)
