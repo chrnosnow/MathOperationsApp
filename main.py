25a71aaaac7bfd1fb5028cab24d96121a4744dac
Alex-Baba <alexbaba18@gmail.com>
2025-07-22 10:48:00 +0300

    create API endpoints for CRUD

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from db import create_db_and_tables
+from db import get_session
 from contextlib import asynccontextmanager
+from sqlmodel import Session, select
+from models.log import RequestLog
 
 
 #test for database connection and creation
@@ -20,3 +23,15 @@ app = FastAPI(lifespan=lifespan)
 @app.get("/")
 def read_root():
     return {"message": "Hello, World!"}
+
+@app.post("/logs/", response_model=RequestLog)
+def create_log(log: RequestLog, session: Session = Depends(get_session)):
+    session.add(log)
+    session.commit()
+    session.refresh(log)
+    return log
+
+@app.get("/logs/", response_model=list[RequestLog])
+def read_logs(session: Session = Depends(get_session)):
+    logs = session.exec(select(RequestLog)).all()
+    return logs
